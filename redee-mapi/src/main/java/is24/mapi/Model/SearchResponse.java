eeef5d4191cecae65ed13a27323f330adf96fff4
Oleksii Ustenko <oleksii.ustenko@scout24.com>
2018-08-28 09:20:37 +0200

    added missing fields to SearchResponse

--- redee-mapi/src/main/java/is24/mapi/Model/SearchResponse.java
+++ redee-mapi/src/main/java/is24/mapi/Model/SearchResponse.java
@@ -4,9 +4,19 @@ import java.util.List;
 
 public class SearchResponse {
 
-  public final List<SearchItem> results;
+    public final List<SearchItem> results;
+    public final int totalResults;
+    public final int pageSize;
+    public final int pageNumber;
+    public final int numberOfPages;
+    public final int totalNewResults;
 
-  public SearchResponse(List<SearchItem> results) {
-    this.results = results;
-  }
+    public SearchResponse(List<SearchItem> results, int totalResults, int pageSize, int pageNumber, int numberOfPages, int totalNewResults) {
+        this.results = results;
+        this.totalResults = totalResults;
+        this.pageSize = pageSize;
+        this.pageNumber = pageNumber;
+        this.numberOfPages = numberOfPages;
+        this.totalNewResults = totalNewResults;
+    }
 }
