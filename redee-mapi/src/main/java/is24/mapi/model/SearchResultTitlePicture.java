d8c861304630d23c2fd7b65ef44415160e0e29d8
Oleksii Ustenko <oleksii.ustenko@scout24.com>
2018-08-30 09:53:20 +0200

    fixed visibility

--- redee-mapi/src/main/java/is24/mapi/model/SearchResultTitlePicture.java
+++ redee-mapi/src/main/java/is24/mapi/model/SearchResultTitlePicture.java
@@ -1,8 +1,8 @@
 package is24.mapi.model;
 
 public class SearchResultTitlePicture {
-    private final String preview;
-    private final String full;
+    public final String preview;
+    public final String full;
 
     public SearchResultTitlePicture(String preview, String full) {
         this.preview = preview;
